1ed5c5fcb545deeb25e437d1c7d18c829d1b1eef
LaraBMenezes <larabarbosabgdemenezes@gmal.com>
2025-01-18 20:07:31 -0300

    Adição de função para sertion de servicos

--- frontend/client_system/initial_page/main.js
+++ frontend/client_system/initial_page/main.js
@@ -1,7 +1,9 @@
 // Caminho para o arquivo JSON
 const pacotesJson = "../../json/pacotes-hotel.json";
+const servicosJson = "../../json/servicos.json";
 
 const container = document.querySelector('.pacotes');
+const atracoes = document.querySelector('.atracoes');
 
 // Função para carregar e renderizar os pacotes
 async function carregarPacotes() {
@@ -17,7 +19,7 @@ async function carregarPacotes() {
 
         // Convertendo os dados para JSON
         const pacotes = await response.json();
-        
+
 
         // Iterando sobre os pacotes e adicionando ao HTML
         pacotes.forEach(pacote => {
@@ -50,3 +52,49 @@ function comprarPacote(id) {
 
 // Carregar os pacotes ao carregar a página
 carregarPacotes();
+
+async function carregarServicos() {
+    try {
+        // Carregando os dados do arquivo JSON
+        const response = await fetch(servicosJson);
+
+
+        // Verificando se o arquivo foi encontrado
+        if (!response.ok) {
+            throw new Error(`Erro ao carregar o arquivo JSON: ${response.statusText}`);
+        }
+
+        // Convertendo os dados para JSON
+        const servicos = await response.json();
+
+        for(let i = 0; i < servicos.lenght; i++){
+            const classe = i % 2;
+            const servicoHTML =  `
+                <div class="atracao${classe}">
+                    <img src="../img/servicos/${servicos[i].imagem}" alt="${servicos[i].nome}">
+                    <div>
+                        <h3>${servicos[i].nome}</h3>
+                        <p>${servicos[i].descricao}</p>
+                        <button onclick="comprarServico(${servicos[i].id})">
+                            Comprar
+                        </button>
+                    </div>
+                </div>
+            `;
+            container.innerHTML += servicoHTML;
+        }
+
+
+    } catch (error) {
+        console.error(error);
+        container.innerHTML = `<p>Erro ao carregar os servicos. Tente novamente mais tarde.</p>`;
+    }
+}
+
+// Função chamada ao clicar no botão "Comprar"
+function comprarServico(id) {
+    alert(`Serviços ${id} selecionado para compra!`);
+}
+
+// Carregar os pacotes ao carregar a página
+carregarServicos();
